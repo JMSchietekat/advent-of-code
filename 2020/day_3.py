9dc387dd1f4bed7f7b9f7e94fbf4385e6d21e6cd
Justin Schietekat <justinschietekat@gmail.com>
2020-12-07 23:33:39 +0200

    Complete part 2 of day 3.

--- 2020/day_3.py
+++ 2020/day_3.py
@@ -33,13 +33,13 @@ def traverseAndCount(arr, right, down):
         if down * step < height:
             if arr[r][c] == '.':
                 openSquareCount += 1
-                arr[r][c] = '0'
+                # arr[r][c] = '0'
             if arr[r][c] == '#':
                 treeCount += 1
-                arr[r][c] = 'X'
+                # arr[r][c] = 'X'
 
             step += 1
-        print(line)
+        # print(line)
 
     return treeCount, openSquareCount
 
@@ -47,4 +47,12 @@ def traverseAndCount(arr, right, down):
 if __name__ == "__main__":
     arr = fileTo2DArr(inputFilePath)
 
-    print(traverseAndCount(arr, 3, 1))
+    print("Part 1: Traversing right 3 and down 1 would encounter {} trees".format(traverseAndCount(arr, 3, 1)[0]))
+
+    product = traverseAndCount(arr, 1, 1)[0] 
+    product *= traverseAndCount(arr, 3, 1)[0] 
+    product *= traverseAndCount(arr, 5, 1)[0] 
+    product *= traverseAndCount(arr, 7, 1)[0] 
+    product *= traverseAndCount(arr, 1, 2)[0] 
+
+    print("Part 2: Traversing product is {}".format(product))
