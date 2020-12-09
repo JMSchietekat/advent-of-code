99d82448906c7385e22e7331b380ad429985d7a9
Justin Schietekat <justinschietekat@gmail.com>
2020-12-09 23:43:38 +0200

    Complete part 2 of day 8.

--- 2020/day_8.py
+++ 2020/day_8.py
@@ -1,7 +1,8 @@
 inputFilePath = "C:/git-jmschietekat/advent-of-code/2020/day_8_input.txt"
 
+
 def mapDay8(text):
-    cmd, arg = (text).replace('\n','').split(' ')
+    cmd, arg = (text).replace('\n', '').split(' ')
 
     if cmd == 'acc':
         cmd = acc
@@ -13,47 +14,95 @@ def mapDay8(text):
     return cmd, int(arg), 0
 
 
-def nop(instructions, programCounter, accumulator):
-    instructions[programCounter] = (instructions[programCounter][0], instructions[programCounter][1], 1)
-    programCounter += 1    
-    
-    if instructions[programCounter][2] == 1:
-        print('Part 1: Start of infinte loop at program counter: {}, accumulator: {}, command: {}, argument: {}'.format(programCounter, accumulator, instructions[programCounter][0].__name__, instructions[programCounter][1]))
+def nop(program, stack, accumulator):
+    pc = stack[-1]
+
+    if program[pc][2] == 1:
+        failWith(program, stack, accumulator)
         return
 
-    instructions[programCounter][0](instructions, programCounter, accumulator)
+    program[pc] = (program[pc][0], program[pc][1], 1)
+    stack.append(pc + 1)
 
-
-def jmp(instructions, programCounter, accumulator):
-    instructions[programCounter] = (instructions[programCounter][0], instructions[programCounter][1], 1)
-    programCounter += instructions[programCounter][1]
-    
-    if instructions[programCounter][2] == 1:
-        print('Part 1: Start of infinte loop at program counter: {}, accumulator: {}, command: {}, argument: {}'.format(programCounter, accumulator, instructions[programCounter][0].__name__, instructions[programCounter][1]))
+    if stack[-1] >= len(program):
+        print("Terminated, accumulator: {}".format(accumulator))
         return
 
-    instructions[programCounter][0](instructions, programCounter, accumulator)
+    program[stack[-1]][0](program, stack, accumulator)
 
-def acc(instructions, programCounter, accumulator):
-    instructions[programCounter] = (instructions[programCounter][0], instructions[programCounter][1], 1)
-    accumulator += instructions[programCounter][1]
-    programCounter += 1    
-    
-    if instructions[programCounter][2] == 1:
-        print('Part 1: Start of infinte loop at program counter: {}, accumulator: {}, command: {}, argument: {}'.format(programCounter, accumulator, instructions[programCounter][0].__name__, instructions[programCounter][1]))
+
+def jmp(program, stack, accumulator):
+    pc = stack[-1]
+
+    if program[pc][2] == 1:
+        failWith(program, stack, accumulator)
         return
 
-    instructions[programCounter][0](instructions, programCounter, accumulator)
+    program[pc] = (program[pc][0], program[pc][1], 1)
+    stack.append(pc + program[pc][1])
+
+    if stack[-1] >= len(program):
+        print("Terminated, accumulator: {}".format(accumulator))
+        return
+
+    program[stack[-1]][0](program, stack, accumulator)
+
+
+def acc(program, stack, accumulator):
+    pc = stack[-1]
+
+    if program[pc][2] == 1:
+        failWith(program, stack, accumulator)
+        return
+
+    program[pc] = (program[pc][0], program[pc][1], 1)
+    accumulator += program[pc][1]
+    stack.append(pc + 1)
+
+    if stack[-1] >= len(program):
+        print("Terminated, accumulator: {}".format(accumulator))
+        return
+
+    program[stack[-1]][0](program, stack, accumulator)
+
+
+def failWith(program, stack, accumulator):
+    print('Start of recursion, accumulator: {}'.format(accumulator))
+    # for i, pc in enumerate(stack[::-1]):
+    #     print('{}: {}'.format(i, intructionToString(program, pc)))
+    #     if i > 10:
+    #         break
+
+def intructionToString(program, pc):
+    return ("program counter: {}, command: {}, argument: {}".format(pc, program[pc][0].__name__, program[pc][1]))
+
+def swopCommand(program, pos):
+    copyProgram = program[:]
+
+    if copyProgram[pos][0] == nop:
+        copyProgram[pos] = (jmp, copyProgram[pos][1], 0)
+        return copyProgram
+
+    if copyProgram[pos][0] == jmp:
+        copyProgram[pos] = (nop, copyProgram[pos][1], 0)
+        return copyProgram
+
+    return None
+
+def run(program):
+    if program:
+        stack = [0]
+        accumulator = 0
+        program[stack[-1]][0](program, stack, accumulator)
 
 if __name__ == "__main__":
-    instructions = [mapDay8(line) for line in open(inputFilePath, "r")]
-    programCounter = 0
-    accumulator = 0
-
-    instructions[programCounter][0](instructions, programCounter, accumulator)
-    print('done')
-
-
+    program = [mapDay8(line) for line in open(inputFilePath, "r")]
     
+    print("Part 1")
+    run(program)
 
-    
+    program = [mapDay8(line) for line in open(inputFilePath, "r")]
+
+    print("Part 2")
+    for itr in range(len(program)):
+        run(swopCommand(program, itr))
