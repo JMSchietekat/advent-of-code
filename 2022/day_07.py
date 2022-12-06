1116857c0aa8d801064ed20345c184b6601aa77d
JMSchietekat <justinschietekat@gmail.com>
2022-12-06 22:34:43 +0200

    Prep for day 7 and an empty day in advance.

---
+++ 2022/day_07.py
@@ -0,0 +1,20 @@
+import os
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) 
+SAMPLE_INPUT_PATH = ROOT_DIR + '/day_07_input_sample.txt'
+INPUT_PATH = ROOT_DIR + "/day_07_input.txt"
+
+def p1(path):
+    return 0
+
+def p2(path):
+    return 0
+
+
+if __name__ == "__main__":
+
+    print(p1(SAMPLE_INPUT_PATH))
+    print(p1(INPUT_PATH))
+
+    print(p2(SAMPLE_INPUT_PATH))
+    print(p2(INPUT_PATH))
