92adfe15d7212b37bb5f7fe32f011f87212f0a17
JMSchietekat <justinschietekat@gmail.com>
2022-12-08 22:39:04 +0200

    Day 8

--- 2022/day_08.py
+++ 2022/day_08.py
@@ -5,10 +5,101 @@ SAMPLE_INPUT_PATH = ROOT_DIR + '/day_08_input_sample.txt'
 INPUT_PATH = ROOT_DIR + "/day_08_input.txt"
 
 def p1(path):
-    return 0
+    
+    M = []
+
+    with open(path) as f:
+        for l in f:
+            R = []
+            for i in l.strip():
+                R.append([int(i),0])
+            M.append(R)
+
+    W = len(M)
+    H = len(M[0])
+    
+    for c in range(1,H-1):
+        for r in range(1,W-1):
+            for ri in range(r+1,W):
+                if M[r][c][0] <= M[ri][c][0]:
+                    break 
+                elif ri == W-1:
+                    M[r][c][1] += 1
+            
+            for ci in range(c+1,H):
+                if M[r][c][0] <= M[r][ci][0]:
+                    break 
+                elif ci == H-1:
+                    M[r][c][1] += 1
+
+            for ri in range(r-1,-1,-1):
+                if M[r][c][0] <= M[ri][c][0]:
+                    break 
+                elif ri == 0:
+                    M[r][c][1] += 1
+            
+            for ci in range(c-1,-1,-1):
+                if M[r][c][0] <= M[r][ci][0]:
+                    break 
+                elif ci == 0:
+                    M[r][c][1] += 1 
+                    
+
+    score = 2 * (H-1) + 2 * (W-1)
+
+    for r in range(1,H-1):
+        for c in range(1,W-1):
+            if M[r][c][1] > 0:
+                score += 1
+    
+    return score
 
 def p2(path):
-    return 0
+    M = []
+
+    with open(path) as f:
+        for l in f:
+            R = []
+            for i in l.strip():
+                R.append([int(i),0])
+            M.append(R)
+
+    W = len(M)
+    H = len(M[0])
+    
+    for c in range(1,H-1):
+        for r in range(1,W-1):
+            s = [0,0,0,0]
+            for ri in range(r+1,W):
+                s[0] += 1
+                if M[r][c][0] <= M[ri][c][0]:
+                    break                
+            
+            for ci in range(c+1,H):
+                s[1] += 1
+                if M[r][c][0] <= M[r][ci][0]:
+                    break
+
+            for ri in range(r-1,-1,-1):
+                s[2] += 1 
+                if M[r][c][0] <= M[ri][c][0]:
+                    break
+            
+            for ci in range(c-1,-1,-1):
+                s[3] += 1 
+                if M[r][c][0] <= M[r][ci][0]:
+                    break
+            
+            M[r][c][1] = s[0] * s[1] * s[2] * s[3]
+                    
+    score = 0    
+
+    for r in range(1,H-1):
+        for c in range(1,W-1):
+            if M[r][c][1] > score:
+                score = M[r][c][1]
+    
+    return score
 
 
 if __name__ == "__main__":
